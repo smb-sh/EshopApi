8a89610b3938924bbe4042a2a5c50d7d86ee3e95
smb-sh <smbsh20@gmail.com>
2023-10-13 23:31:17 +0330

    database connection fixed

--- EshopApi/Program.cs
+++ EshopApi/Program.cs
@@ -1,3 +1,6 @@
+using EshopApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<EshopApiDbContext>(options => 
+{
+    options.UseSqlServer("Data Source=.;Initial Catalog=EshopApi_DB;Integrated Security=True;TrustServerCertificate=true;");
+});
 
 var app = builder.Build();
 
