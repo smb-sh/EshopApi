68b209b10e537d23752f74a4e3098d9951a7d036
smb-sh <smbsh20@gmail.com>
2023-10-16 00:15:41 +0330

    simple CRUD api for customer

---
+++ EshopApi/Controllers/CustomersController.cs
@@ -0,0 +1,57 @@
+﻿using EshopApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EshopApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private EshopApiDbContext _context;
+
+        public CustomersController(EshopApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCustomer()
+        {
+            return new ObjectResult(_context.Customers);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCustomer([FromRoute] int id)
+        {
+            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.CustomerId == id);
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
+        {
+            _context.Customers.Add(customer);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer([FromRoute] int id, [FromBody] Customer customer)
+        {
+            _context.Entry(customer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(customer);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer([FromRoute] int id)
+        {
+            var customer = await _context.Customers.FindAsync(id);
+            _context.Customers.Remove(customer);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
