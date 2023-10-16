8c5e7db1948aa3df8f9b63e97cb6d06fa0d90cbc
smb-sh <smbsh20@gmail.com>
2023-10-16 16:44:03 +0330

    add validation to customer controller & status codes

--- EshopApi/Controllers/CustomersController.cs
+++ EshopApi/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace EshopApi.Controllers
 {
@@ -19,19 +20,46 @@ namespace EshopApi.Controllers
         [HttpGet]
         public IActionResult GetCustomer()
         {
-            return new ObjectResult(_context.Customers);
+            var result = new ObjectResult(_context.Customer)
+            {
+                // set status code
+                StatusCode = (int)HttpStatusCode.OK
+            };
+            // adding header
+            Request.HttpContext.Response.Headers.Add("X-Count", _context.Customer.Count().ToString());
+
+            return result;
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetCustomer([FromRoute] int id)
+        {   
+            // check user exist or not
+            if (CustomerExists(id))
+            {
+                var customer = await _context.Customer.SingleOrDefaultAsync(c => c.CustomerId == id);
+                return Ok(customer);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+        
+        private bool CustomerExists(int id)
         {
-            var customer = await _context.Customers.SingleOrDefaultAsync(c => c.CustomerId == id);
-            return Ok(customer);
+            return _context.Customer.Any(c => c.CustomerId == id);
         }
 
         [HttpPost]
         public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
         {
+            // check validation of model 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
             _context.Customers.Add(customer);
             await _context.SaveChangesAsync();
             return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
